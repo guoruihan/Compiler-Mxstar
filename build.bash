c4e43082724ccd0ad08ba1eac626bc00d5f670f5
guoruihan <43490455+guoruihan@users.noreply.github.com>
2020-04-05 21:45:13 +0800

    Update build.bash

--- build.bash
+++ build.bash
@@ -4,4 +4,4 @@
 set -e
 cd "$(dirname "$0")"
 mkdir -p bin
-find -name *.java | javac -d bin -classpath "./lib/antlr-4.8-complete.jar" @/dev/stdin
+find . -name *.java | javac -d bin -classpath "./lib/antlr-4.8-complete.jar" @/dev/stdin
